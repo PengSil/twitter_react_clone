c323f890fe61cff1ad1fc5b7e6a161a6c94d6913
MoonSung <choimoon973@gmail.com>
2023-11-26 04:42:22 +0900

    back작업 시작

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,16 @@
 const http = require("http");
 
-http.createServer((req, res) => {});
-http.listen(3065);
+// createServer 라는 걸로 요청을 받고 응답을 해준다
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.write("Hello");
+  res.write("Hello");
+  res.write("Hello");
+  res.write("Hello");
+  res.write("Hello");
+  res.end("Hello node");
+});
+
+server.listen(3065, () => {
+  console.log("서버 실행 중");
+});
